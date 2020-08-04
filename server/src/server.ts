cbeef7fd8be411deb29bf3f47f116620026997f7
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-08-04 12:26:33 -0300

    project create and configured

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,11 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  return response.json('Hello World');
+app.use(express.json());
+
+app.get('/', (request, response) => {
+  return response.json({message: 'Hello World'});
 });
+
 
 app.listen(3333);
