322886c3429123cbe602c22bb06d100b08349003
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-08-04 15:02:41 -0300

    create-connection route created

--- server/src/database/migrations/03_create_connection.ts
+++ server/src/database/migrations/03_create_connection.ts
@@ -2,9 +2,13 @@ import Knex from 'knex';
 
 export async function up(knex: Knex) {
   return knex.schema.createTable('connections', table => {
-    table.increments('id').primary();
+    table
+      .increments('id')
+      .primary()
+    ;
 
-    table.integer('user_id')
+    table
+      .integer('user_id')
       .notNullable()
       .references('id')
       .inTable('users')
@@ -12,8 +16,9 @@ export async function up(knex: Knex) {
       .onDelete('CASCADE')
     ;
     
-    table.timestamp('created_at')
-      .defaultTo('now()')
+    table
+      .timestamp('created_at')
+      .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
       .notNullable()
     ;
   });
