648a08bb6a8cca7b8046ade0f18dc4471bb14b30
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-08-07 20:27:43 -0300

    teachers filters created and styled

--- mobile/src/pages/TeacherList/styles.ts
+++ mobile/src/pages/TeacherList/styles.ts
@@ -8,7 +8,59 @@ const styles = StyleSheet.create({
   },
 
   teacherList: {
-    marginTop: -60,
+    marginTop: -40,
+  },
+
+  searchForm: {
+    marginBottom: 24,
+  },
+
+  label: {
+    color: '#D4C2FF',
+
+    fontFamily: 'Poppins_400Regular'
+  },
+
+  inputGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  inputBlock: {
+    width: '48%',
+  },
+
+  input: {
+    justifyContent: 'center',
+
+    height: 54,
+
+    marginTop: 4,
+    marginBottom: 16,
+    paddingHorizontal: 16,
+    
+    backgroundColor: '#FFF',
+    
+    borderRadius: 8,
+  },
+
+  submitButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    height: 56,
+
+    backgroundColor: '#04D361',
+
+    borderRadius: 8,
+  },
+
+  submitButtonText: {
+    color: '#FFF',
+
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 16,
   },
 });
 
