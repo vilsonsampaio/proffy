c853306e425c3b7ae69d8362a228487f29802fa6
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-08-07 21:38:54 -0300

    favorites added and removed

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, ScrollView, Text, TextInput } from 'react-native';
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
 import { Feather } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import api from '../../services/api';
  
@@ -12,18 +13,34 @@ import styles from './styles';
 
 const TeacherList = () => {
   const [isFiltersVisible, setIsFiltersVisible] = useState(false);
-
+  
   const [subject, setSubject] = useState('');
   const [week_day, setWeekDay] = useState('');
   const [time, setTime] = useState('');
   
   const [teachers, setTeachers] = useState([]);
+  const [favorites, setFavorites] = useState<number[]>([]);
+
+  function loadFavorites() {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+        const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => {
+          return teacher.id;
+        });
+
+        setFavorites(favoritedTeachersIds);
+      }
+    });
+  }
 
   function handleToggleFiltersVisible() {
     setIsFiltersVisible(!isFiltersVisible);
   }
 
   async function handleFiltersSubmit() {
+    loadFavorites();
+    
     const response = await api.get('classes', {
       params: {
         subject,
@@ -98,7 +115,13 @@ const TeacherList = () => {
         }}
       >
         {teachers.map((teacher: Teacher) => {
-          return <TeacherItem key={teacher.id}  teacher={teacher} />
+          return (
+            <TeacherItem 
+              key={teacher.id}
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)} 
+            />
+          );
         })}
       </ScrollView>
     </View>
