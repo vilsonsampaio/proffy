9b45e5fec731d3ac11f213ae28ada5b28f85eff1
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-08-18 15:54:56 -0300

    :truck: chore: add auth middleware

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+import authConfig from '../config/auth';
+
+export default async (request: Request, response: Response, next: NextFunction) => {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).json({ error: 'Tolken not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+    
+  try {
+    const decode: any = await jwt.verify(token, authConfig.secret);
+    
+    request.userId = decode.id;
+    
+    return next();
+  } catch (error) {
+    console.log(error);
+
+    return response.status(401).json({ error: 'Token invalid' })
+  }
+}
