6c915eba0bafa8f278670e9aabb91075eecccef2
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-08-25 12:12:44 -0300

    :goal_net: chore: catch errors

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -6,26 +6,33 @@ export default class ConnectionsController {
   async store(request: Request, response: Response) {
     const { user_id } = request.body;
 
-    await db('connections')
-      .insert({
-        user_id,
-      })
-    ;
+    try {
+      await db('connections')
+        .insert({
+          user_id,
+        })
+      ;
 
-    return response.status(201).send();
+      return response.status(201).send();
+    } catch (error) {
+      console.log(error);
+      
+      return response.status(401).json({ error: 'Unexpected error while adding new connection' });
+    }
   }
   
   async show(request: Request, response: Response) {
-    const { total_connections } = await db('connections')
-      .count('* as total_connections')
-      .first()
-    ;
-
-    const { total_teachers } = await db('classes')
-      .countDistinct('user_id as total_teachers')
-      .first()
-    ;
-
-    return response.json({ total_connections, total_teachers });
+    try {
+      const { total_connections } = await db('connections')
+        .count('* as total_connections')
+        .first()
+      ;
+      
+      return response.json({ total_connections });
+    } catch (error) {
+      console.log(error);
+      
+      return response.status(401).json({ error: 'Unexpected error while getting total of connections' });
+    }
   }
 }
