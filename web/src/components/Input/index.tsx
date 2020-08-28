5f3f6d4229b9d2d7203c3d38e8aacfcf0f61a658
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-08-27 21:13:35 -0300

    :sparkles: feat: update input component

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -1,18 +1,57 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { useState } from 'react';
+import { useTheme } from 'styled-components';
+import { FiEye, FiEyeOff } from 'react-icons/fi';
 
-import './styles.css';
+import { InputProps } from './Input';
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  name: string;
-  label: string;
-}
+import { Container } from './styles';
 
-const Input: React.FC<InputProps> = ({ name, label, ...rest }) => {
+
+const Input: React.FC<InputProps> = ({ variant = 'default', type = 'text', name, label, ...rest }) => {
+  const [visibility, setVisibility] = useState(false);
+
+  const { colors } = useTheme();
+
   return (
-    <div className="input-block">
-      <label htmlFor={name}>{label}</label>
-      <input type="text" id={name} {...rest} />
-    </div>
+    <Container variant={variant}>
+      {variant !== 'sessions' 
+        ? <label htmlFor={name}>{label}</label>
+        : '' 
+      }
+      
+      <input 
+        type={
+          type === 'password'
+            ? visibility ? 'text' : 'password'
+            : `${type}`
+        } 
+        id={name} 
+        placeholder={
+          variant === 'sessions'
+            ? label
+            : ''
+        } 
+        {...rest} 
+      />
+
+      {variant === 'sessions' 
+        ? <label htmlFor={name}>{label}</label>
+        : '' 
+      }
+
+      {type === 'password' 
+        ? 
+          (
+            <button type="button" onClick={() => setVisibility(!visibility)}>
+              {visibility 
+                ? <FiEyeOff color={colors.primary} /> 
+                : <FiEye />
+              }
+            </button>
+          )
+        : ''
+      }
+    </Container>
   );
 }
 
