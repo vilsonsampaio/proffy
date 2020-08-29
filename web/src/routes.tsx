c9b1ecf062119dbf1743bf7ab74ca101f92e8502
vilsonsampaio <contato.vilsonsampaio@gmail.com>
2020-08-29 13:55:41 -0300

    :sparkles: feat: create reset password page

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import SignIn from './pages/Sessions/SignIn';
 import SignUp from './pages/Sessions/SignUp';
 import ForgotPassword from './pages/Sessions/ForgotPassword';
+import ResetPassword from './pages/Sessions/ResetPassword';
 
 
 import Landing from './pages/Landing';
@@ -16,6 +17,7 @@ const Routes = () => {
       <Route path="/sessions/sign-in" component={SignIn} />
       <Route path="/sessions/sign-up" component={SignUp} />
       <Route path="/sessions/forgot-password" component={ForgotPassword} />
+      <Route path="/sessions/reset-password" component={ResetPassword} />
 
 
       <Route path="/" exact component={Landing} />
